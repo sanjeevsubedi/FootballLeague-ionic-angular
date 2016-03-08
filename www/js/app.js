6bfa37101e294eafd9fa3ce990439ef1c7faa3a3
sanjeev subedi <sanjeev@sanjeev-mac.local>
2016-03-08 07:19:37 -0500

    project pushed

---
+++ www/js/app.js
@@ -0,0 +1,111 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('fl', ['ionic', 'fl.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+.state('seasons', {
+    url: '/seasons',
+    templateUrl: 'templates/seasons.html',
+    controller: 'SeasonController'
+
+  })
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html',
+    controller: 'SeasonController'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.schedule', {
+    url: '/schedule/:seasonId/:roundId',
+    views: {
+      'tab-schedule': {
+        templateUrl: 'templates/tab-schedule.html',
+        controller: 'ScheduleController'
+      }
+    }
+  })
+
+  .state('tab.standings', {
+      url: '/standings',
+      views: {
+        'tab-standings': {
+          templateUrl: 'templates/tab-standings.html',
+          controller: 'StandingsController'
+        }
+      }
+    })
+
+    .state('tab.teams', {
+      url: '/teams',
+      views: {
+        'tab-teams': {
+          templateUrl: 'templates/tab-teams.html',
+          controller: 'TeamController'
+        }
+      }
+    })
+
+    .state('tab.players', {
+      url: '/teams/:id/:name',
+      views: {
+        'tab-teams': {
+          templateUrl: 'templates/tab-players.html',
+          controller: 'TeamController'
+        }
+      }
+    })
+
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'templates/settings.html',
+      controller: 'SettingsController'
+       
+    })
+
+    .state('teams', {
+      url: '/teams',
+      templateUrl: 'templates/teams.html',
+      controller: 'TeamController'
+       
+    });
+
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/seasons');
+
+});
---
+++ www/js/services/Loader.js
@@ -0,0 +1,15 @@
+angular.module('fl.loader', [])
+
+.factory('LoadingService', function($http, $ionicLoading) {
+  return {
+    show: function(email,password) {
+	  $ionicLoading.show({
+	      template: '<h4>Loading...</h4>'
+	    });
+    },
+    hide: function(chat) {
+      $ionicLoading.hide();
+    },
+    
+  };
+});
